270169af20bad11f1fb546e1df9b535ce4de9c2c
Donquixote Sabo <mixboksu@gmail.com>
2021-05-22 18:56:23 +0200

    feat: :sparkles: Add navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,24 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  *,*::before,*::after {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+  body {
+   font-size: 14px;
+   background: hsl(218, 28%, 13%);
+   font-family: 'Open Sans', sans-serif;
+  }
+`;
 
 const theme = {
   colors: {
     primary: '#0070f3',
+    neutral: 'hsl(0, 0%, 100%)',
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +28,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+
+import Navigation from '../components/Navigation';
+
 // import styled from 'styled-components'
 // const Title = styled.h1`
 //   font-size: 50px;
@@ -11,8 +14,13 @@ export default function Home() {
       <Head>
         <title>Fylo dark theme landing page</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Raleway:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <h1>SIEMA</h1>
+      <Navigation />
     </>
   );
 }
