c96f0dcf5aaad0f9233e2cec9ca5c22b5d1acd07
Donquixote Sabo <mixboksu@gmail.com>
2021-05-22 18:25:02 +0200

    build: :building_construction: Add eslint and prettier

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Head from 'next/head';
+// import styled from 'styled-components'
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
 
 export default function Home() {
   return (
     <>
-    <Title>SIEMA</Title>
+      <Head>
+        <title>Fylo dark theme landing page</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1>SIEMA</h1>
     </>
-  )
+  );
 }
