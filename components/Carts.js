94dfea1b42bb719635c04b6f5f7872df0a7ecec6
Donquixote Sabo <mixboksu@gmail.com>
2021-05-23 14:41:37 +0200

    feat: :bento: Add correct content

--- components/Carts.js
+++ components/Carts.js
@@ -27,28 +27,33 @@ export default function Carts() {
         content="The ability to use a smartphone, tablet, or computer to access your account means your
         files follow you everywhere."
         img="/images/icon-access-anywhere.svg"
-        alt="alt"
+        alt="Phone and computer"
       />
       <CartsItem
-        title="Access your files, anywhere"
-        content="The ability to use a smartphone, tablet, or computer to access your account means your
-        files follow you everywhere."
-        img="/images/icon-access-anywhere.svg"
-        alt="alt"
+        title="
+        Security you can trust"
+        content="2-factor authentication and user-controlled encryption are just a couple of the security
+        features we allow to help secure your files."
+        img="/images/icon-security.svg"
+        alt="Shield"
       />
       <CartsItem
-        title="Access your files, anywhere"
-        content="The ability to use a smartphone, tablet, or computer to access your account means your
-        files follow you everywhere."
-        img="/images/icon-access-anywhere.svg"
-        alt="alt"
+        title="
+        Real-time collaboration"
+        content="
+        Securely share files and folders with friends, family and colleagues for live collaboration.
+        No email attachments required."
+        img="/images/icon-collaboration.svg"
+        alt="Clock"
       />
       <CartsItem
-        title="Access your files, anywhere"
-        content="The ability to use a smartphone, tablet, or computer to access your account means your
-        files follow you everywhere."
-        img="/images/icon-access-anywhere.svg"
-        alt="alt"
+        title="Store any type of file"
+        content="
+        Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all
+        file types to be securely stored and shared.
+        "
+        img="/images/icon-any-file.svg"
+        alt="Folder"
       />
     </CartsWrapper>
   );
